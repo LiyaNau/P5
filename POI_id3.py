9df375300f47ba0fe0607c3443f1b5f3f4959ef4
LiyaNau <liyanaumova@gmail.com>
2016-07-20 22:23:40 +0700

    cl3 scoring - recall

--- POI_id3.py
+++ POI_id3.py
@@ -8,14 +8,15 @@ sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 from sklearn.grid_search import GridSearchCV
+from sklearn.metrics import f1_score
 
-features_list = ['poi', 'exercised_stock_options', 'total_stock_value', 'bonus', 'salary', 'deferred_income']
-# features_list_all = ['salary',"bonus","to_messages",'total_payments','exercised_stock_options',
-#                  'shared_receipt_with_poi', 'restricted_stock_deferred', 'total_stock_value', 'expenses',
-#                  'loan_advances', 'from_messages', 'other', 'from_this_person_to_poi', 'director_fees',
-#                  'deferred_income', 'long_term_incentive', 'from_poi_to_this_person']
-# features_list = ["poi"]
-# features_list += features_list_all
+# features_list = ['poi', 'exercised_stock_options', 'total_stock_value', 'bonus', 'salary', 'deferred_income']
+features_list_all = ['salary',"bonus","to_messages",'total_payments','exercised_stock_options',
+                 'shared_receipt_with_poi', 'restricted_stock_deferred', 'total_stock_value', 'expenses',
+                 'loan_advances', 'from_messages', 'other', 'from_this_person_to_poi', 'director_fees',
+                'deferred_income', 'long_term_incentive', 'from_poi_to_this_person']
+features_list = ["poi"]
+features_list += features_list_all
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
     data_dict = pickle.load(data_file)
@@ -30,11 +31,6 @@ my_dataset = data_dict
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
 
-### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
 # from sklearn.cross_validation import train_test_split
@@ -44,31 +40,24 @@ labels, features = targetFeatureSplit(data)
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.cross_validation import StratifiedShuffleSplit
 
-cv = StratifiedShuffleSplit(labels,n_iter = 20, test_size=0.2)
-clf = RandomForestClassifier()
+cv = StratifiedShuffleSplit(labels,n_iter = 30, test_size=0.2)
+clf = RandomForestClassifier(class_weight="balanced")
 
 param_grid = {'n_estimators':[10,50,100,200],
-              'max_features':[5,4,3,2,'auto']}
-grid = GridSearchCV(clf,param_grid,verbose=True, cv=cv)
+              'max_features':[10,5,2]}
+grid = GridSearchCV(clf, param_grid ,verbose=True, cv=cv, scoring = 'recall' )
 grid.fit(features,labels)
 print "best estimator:", grid.best_estimator_
+print "best score:", grid.best_score_
 clf = grid.best_estimator_
 
-### Task 5: Tune your classifier to achieve better than .3 precision and recall
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info:
+
 ### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
 
 # Example starting point. Try investigating other evaluation techniques!
 
-clf.fit(features_train,labels_train)
-print clf.oob_score
+#clf.fit(features_train,labels_train)
+#print clf.oob_score
 
-### Task 6: Dump your classifier, dataset, and features_list so anyone can
-### check your results. You do not need to change anything below, but make sure
-### that the version of poi_id.py that you submit can be run on its own and
-### generates the necessary .pkl files for validating your results.
 
 dump_classifier_and_data(clf, my_dataset, features_list)
