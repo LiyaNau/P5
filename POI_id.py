b579aa08b6503b59ae5d2497ac08cb43a78f83aa
LiyaNau <liyanaumova@gmail.com>
2017-04-30 16:32:50 +0700

    code cleared

--- POI_id.py
+++ POI_id.py
@@ -16,9 +16,7 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.pipeline import Pipeline
 
 
-### Task 1: Select what features you'll use.
-### features_list is a list of strings, each of which is a feature name.
-### The first feature must be "poi".
+
 #features_list = ['poi', 'exercised_stock_options', 'total_stock_value', 'bonus', 'salary', 'deferred_income']
 features_list_all = ['salary',"bonus","to_messages",'total_payments','exercised_stock_options',
                  'shared_receipt_with_poi', 'restricted_stock_deferred', 'total_stock_value', 'expenses',
@@ -26,7 +24,7 @@ features_list_all = ['salary',"bonus","to_messages",'total_payments','exercised_
                  'deferred_income', 'long_term_incentive', 'from_poi_to_this_person']
 features_list = ["poi"]
 
- # You will need to use more features
+
 
 ### Load the dictionary containing the dataset
 with open("final_project_dataset.pkl", "r") as data_file:
@@ -35,7 +33,7 @@ with open("final_project_dataset.pkl", "r") as data_file:
 
 ### Task 2: Remove outliers
 data_dict.pop('TOTAL')
-#data_dict.pop("THE TRAVEL AGENCY IN THE PARK")
+
 
 ### Task 3: Create new feature(s)
 
@@ -56,45 +54,27 @@ for item, value in data_dict.items():
         value['from_fraction'] = value['from_this_person_to_poi']/float(value['from_messages'])
 
 
-# features_list_all.append('fin1')
-# features_list_all.append('to_fraction')
-# features_list_all.append('from_fraction')
+features_list_all.append('fin1')
+features_list_all.append('to_fraction')
+features_list_all.append('from_fraction')
 features_list += features_list_all
 my_dataset = data_dict
 
 ### Extract features and labels from dataset for local testing
 data = featureFormat(my_dataset, features_list, sort_keys = True)
 labels, features = targetFeatureSplit(data)
-# print features[0] + features[1]
-
-
-#selector = SelectKBest(f_classif, k=5)
-# features_list_all.append('fin1')
-# features_list_all.append('to_fraction')
-# features_list_all.append('from_fraction')
-#selector.fit(features, labels)
-#features = selector.transform(features)
-
-#features_df = pd.DataFrame({'feature': features_list_all,
-#                            'score': selector.scores_})
-# features_df.sort_values(by="score", ascending=False, inplace=True)
-# best_features = list(features_df["feature"][0:5])
-# print best_features
-# print features_df
-#
-# features_list = ["poi"] + best_features
-# print features_list
-
 
 cv = StratifiedShuffleSplit(labels,n_iter = 100, test_size=0.4,random_state=45)
 steps = [('skb', SelectKBest()), ('nb', GaussianNB())]
 clf = Pipeline(steps)
 
+### Task 5: Tune your classifier to achieve better than .3 precision and recall
+
 param_grid = {'skb__k':range(1,18)}
 grid = GridSearchCV(clf, param_grid ,verbose=True, cv=cv, scoring = 'f1' )
 grid.fit(features,labels)
 print "best estimator:", grid.best_estimator_
-print "best score:", grid.best_score_
+print "best F1:", grid.best_score_
 clf = grid.best_estimator_
 
 selector = SelectKBest(f_classif, k=5)
@@ -107,43 +87,9 @@ print best_features
 
 features_list = ["poi"] + best_features
 print features_list
-# ### Extract features and labels from dataset for local testing
-# data = featureFormat(my_dataset, features_list, sort_keys = True)
-# labels, features = targetFeatureSplit(data)
-
-### Task 4: Try a varity of classifiers
-### Please name your classifier clf for easy export below.
-### Note that if you want to do PCA or other multi-stage operations,
-### you'll need to use Pipelines. For more info:
-### http://scikit-learn.org/stable/modules/pipeline.html
-
-# # Provided to give you a starting point. Try a variety of classifiers.
-# from sklearn.naive_bayes import GaussianNB
-# clf = GaussianNB()
 
 
-
-### Task 5: Tune your classifier to achieve better than .3 precision and recall
-### using our testing script. Check the tester.py script in the final project
-### folder for details on the evaluation method, especially the test_classifier
-### function. Because of the small size of the dataset, the script uses
-### stratified shuffle split cross validation. For more info:
-### http://scikit-learn.org/stable/modules/generated/sklearn.cross_validation.StratifiedShuffleSplit.html
-
-# Example starting point. Try investigating other evaluation techniques!
-# features_train, features_test, labels_train, labels_test = \
-#     train_test_split(features, labels, test_size=0.2)
-#
-# clf.fit(features_train,labels_train)
-# pred = clf.predict(features_test)
-# print "accuracy: ", clf.score(features_test,labels_test)
-# print "f1;", f1_score(labels_test,pred)
-# print "precision:", precision_score(labels_test,pred)
-# print "recall:", recall_score(labels_test, pred)
-#
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
-### check your results. You do not need to change anything below, but make sure
-### that the version of poi_id.py that you submit can be run on its own and
-### generates the necessary .pkl files for validating your results.
+### check your results.
 
 dump_classifier_and_data(clf, my_dataset, features_list)
